2d99cf337b0449012c666ebd80889cfffd4cfdfd
achwaf <achwaf@gmail.com>
2023-02-08 00:03:39 +0100

    initializing project

---
+++ src/main/java/lu/cascade/assessment/user/manager/api/UserManagerApiApplication.java
@@ -0,0 +1,13 @@
+package lu.cascade.assessment.user.manager.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagerApiApplication.class, args);
+	}
+
+}
